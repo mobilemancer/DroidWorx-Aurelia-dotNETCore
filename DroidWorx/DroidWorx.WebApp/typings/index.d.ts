990edcc21ea3499948b2ab2857a00dedd54f8bc6
Andreas Wänqvist <andrkarl@gmail.com>
2016-08-16 14:12:02 +0200

    Added URL typings

--- DroidWorx/DroidWorx.WebApp/typings/index.d.ts
+++ DroidWorx/DroidWorx.WebApp/typings/index.d.ts
@@ -2,4 +2,5 @@
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/selenium-webdriver/index.d.ts" />
+/// <reference path="globals/url/index.d.ts" />
 /// <reference path="globals/whatwg-fetch/index.d.ts" />
